ed63ca45e2c52435865b2f32d661c8cff902008a
snitovlad <snitovov@gmail.com>
2023-04-11 22:39:25 +0300

    Lesson 20. NavLink

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,23 +1,37 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css'
 
+//Это переменная для класса .active для className в NavLink
+const activeLink = ({ isActive }) => isActive ? s.active : s.item; //здесь вызов класса className={activeLink}
+
+/*const activeLink = () => {   //здесь вызов класса className={activeLink()}
+  return (
+    select => select.isActive ? s.active : s.item
+  );
+}*/
+
+//className={classActive => classActive.isActive ? s.active : s.item}  //или просто так вставить в NavLink
+
 const Navbar = () => {
-   return <nav className={s.nav}>
-   <div className ={`${s.item} ${s.active}`}>
-     <a href="/profile">Profile</a>
-   </div>
-   <div className={s.item}>
-     <a href="/dialogs">Messages</a>
-   </div>
-   <div className={s.item}>
-     <a href="/news">News</a>
-   </div>
-   <div className={s.item}>
-     <a href="/music">Music</a>
-   </div>
-   <div className={s.item}>
-     <a href="/setting">Setting</a>
-   </div>
- </nav>
+  return (
+    <nav className={s.nav}>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/profile" className={activeLink}>Profile</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/dialogs" className={activeLink}>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/news" className={activeLink}>News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" className={activeLink}>Music</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/setting" className={activeLink}>Setting</NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
