3bbf48bc8a3a751af19f4316ca81443595cc5728
snitovlad <snitovov@gmail.com>
2023-04-11 21:31:49 +0300

    Lesson 19. Route, browser-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './componets/Dialogs/Dialogs';
 import Header from './componets/Header/Header';
+import Music from './componets/Music/Music';
 import Navbar from './componets/Navbar/Navbar';
+import News from './componets/News/News';
 import Profile from './componets/Profile/Profile';
+import Setting from './componets/Setting/Setting';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      {/*<Profile />*/}
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/setting" element={<Setting />} />
+
+            <Route path="*" exact element={<Profile />} /> {/*для пути по дефолту если страница не найдена*/}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -3,19 +3,19 @@ import s from './Navbar.module.css'
 const Navbar = () => {
    return <nav className={s.nav}>
    <div className ={`${s.item} ${s.active}`}>
-     <a href="#">Profile</a>
+     <a href="/profile">Profile</a>
    </div>
    <div className={s.item}>
-     <a href="#">Messages</a>
+     <a href="/dialogs">Messages</a>
    </div>
    <div className={s.item}>
-     <a href="#">News</a>
+     <a href="/news">News</a>
    </div>
    <div className={s.item}>
-     <a href="#">Music</a>
+     <a href="/music">Music</a>
    </div>
    <div className={s.item}>
-     <a href="#">Setting</a>
+     <a href="/setting">Setting</a>
    </div>
  </nav>
 }
