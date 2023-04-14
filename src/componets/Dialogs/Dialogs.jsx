fb83f149a16979012b62e719f57f35c5b7aea40f
snitovlad <snitovov@gmail.com>
2023-04-14 23:23:37 +0300

    Lesson 24. Create arrays of Data in Profile and Messages

--- src/componets/Dialogs/Dialogs.jsx
+++ src/componets/Dialogs/Dialogs.jsx
@@ -18,24 +18,42 @@ const Message = (props) => {
    )
 }
 
-const Dialogs = () => {
+const Dialogs = (props) => {
+
+   let dialogsData = [
+      {id: 1, name: 'Dimych'},
+      {id: 2, name: "Andrey"},
+      {id: 3, name: "Sveta"},
+      {id: 4, name: "Sasha"},
+      {id: 5, name: "Victor"},
+      {id: 6, name: "Valera"}
+   ];
+
+   let messagesData = [
+      {id: 1, message: 'Hi!'},
+      {id: 2, message: 'How is your it-kamasutra?'},
+      {id: 3, message: 'Yo!'},
+      {id: 4, message: 'Yo!'},
+      {id: 5, message: 'Yo!'}
+   ]
+
    return (
       <div className={s.dialogs}>
 
          <div className={s.dialogsItems}>
-            <DialogItem id="1" name="Dimych" />
-            <DialogItem id="2" name="Andrey" />
-            <DialogItem id="3" name="Sveta" />
-            <DialogItem id="4" name="Sasha" />
-            <DialogItem id="5" name="Victor" />
-            <DialogItem id="6" name="Valera" />
+            <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+            <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
+            <DialogItem id={dialogsData[2].id} name={dialogsData[2].name} />
+            <DialogItem id={dialogsData[3].id} name={dialogsData[3].name} />
+            <DialogItem id={dialogsData[4].id} name={dialogsData[4].name} />
+            <DialogItem id={dialogsData[5].id} name={dialogsData[5].name} />
          </div>
          <div className={s.messages}>
-            <Message message="Hi!" />
-            <Message message="How is your it-kamasutra?" />
-            <Message message="Yo!" />
-            <Message message="Yo!" />
-            <Message message="Yo!" />
+            <Message message={messagesData[0].message} />
+            <Message message={messagesData[1].message} />
+            <Message message={messagesData[2].message} />
+            <Message message={messagesData[3].message} />
+            <Message message={messagesData[4].message} />
          </div>
 
       </div>
--- src/componets/Profile/MyPosts/MyPosts.jsx
+++ src/componets/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,14 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
 function MyPosts() {
+
+   let postData = [
+      {id: 1, message: 'Hi! How are you?', likeCount: 15, avatar: 'https://pixelbox.ru/wp-content/uploads/2021/04/cats-ava-steam-8.jpg'},
+      {id: 2, message: 'It\'s my first post', likeCount: 20, avatar: 'https://pixelbox.ru/wp-content/uploads/2022/08/avatars-viber-pixelbox.ru-29.jpg'},
+      {id: 3, message: 'I\'m a sportsman. And you?', likeCount: 25, avatar: 'https://sun6-23.userapi.com/s/v1/if1/axZjentIg7fuN9JbKG3sW6Tf3uDApwUE_XzYSuMAbEMue6sJOxRQ6FtVFqqZPO_Q46Ds4ejZ.jpg?size=959x959&quality=96&crop=0,249,959,959&ava=1'},
+
+   ]
+
    return (
       <div className={s.postsBlock}>
          <h3>My posts</h3>
@@ -14,9 +22,9 @@ function MyPosts() {
             </div>
          </div>
          <div className={s.posts}>
-            <Post message="Hi! How are you?" likeCount="15" avatar="https://pixelbox.ru/wp-content/uploads/2021/04/cats-ava-steam-8.jpg" />
-            <Post message="It's my first post" likeCount="20" avatar="https://pixelbox.ru/wp-content/uploads/2022/08/avatars-viber-pixelbox.ru-29.jpg" />
-            <Post message="I'm a sportsman. And you?" likeCount="25" avatar="https://sun6-23.userapi.com/s/v1/if1/axZjentIg7fuN9JbKG3sW6Tf3uDApwUE_XzYSuMAbEMue6sJOxRQ6FtVFqqZPO_Q46Ds4ejZ.jpg?size=959x959&quality=96&crop=0,249,959,959&ava=1" />
+            <Post message={postData[0].message} likeCount={postData[0].likeCount} avatar={postData[0].avatar} />
+            <Post message={postData[1].message} likeCount={postData[1].likeCount} avatar={postData[1].avatar} />
+            <Post message={postData[2].message} likeCount={postData[2].likeCount} avatar={postData[2].avatar} />
 
          </div>
       </div>
