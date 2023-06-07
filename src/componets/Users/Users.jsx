bbaf3b7053e6c0e63a077e6908b6bb524f13db9d
snitovlad <snitovov@gmail.com>
2023-06-07 21:54:24 +0300

    Lesson 52. Side Effect of Users

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -3,33 +3,37 @@ import styles from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
+   let getUsers = () => {
 
-   if (props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-         props.setUsers(response.data.items)
+      if (props.users.length === 0) {
+         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+         })
 
-      })
          //[
-            // {
-            //    id: 1, photoUrl: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg', followed: false, fullName: 'Dmitry K', status: 'I am looking for a job right now',
-            //    location: { country: 'Belarus', city: 'Minsk' }
-            // },
-            // {
-            //    id: 2, photoUrl: 'https://cspromogame.ru//storage/upload_images/avatars/1152.jpg', followed: true, fullName: 'Sasha', status: 'I am so pretty',
-            //    location: { country: 'Russia', city: 'Moskow' }
-            // },
-            // {
-            //    id: 3, photoUrl: 'https://cspromogame.ru//storage/upload_images/avatars/788.jpg', followed: false, fullName: 'Sergey', status: 'I like football',
-            //    location: { country: 'Ukraine', city: 'Kiew' }
-            // }
+         // {
+         //    id: 1, photoUrl: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg', followed: false, fullName: 'Dmitry K', status: 'I am looking for a job right now',
+         //    location: { country: 'Belarus', city: 'Minsk' }
+         // },
+         // {
+         //    id: 2, photoUrl: 'https://cspromogame.ru//storage/upload_images/avatars/1152.jpg', followed: true, fullName: 'Sasha', status: 'I am so pretty',
+         //    location: { country: 'Russia', city: 'Moskow' }
+         // },
+         // {
+         //    id: 3, photoUrl: 'https://cspromogame.ru//storage/upload_images/avatars/788.jpg', followed: false, fullName: 'Sergey', status: 'I like football',
+         //    location: { country: 'Ukraine', city: 'Kiew' }
+         // }
          //]
 
-      
+      }
    }
-   
+
    return (
-      <div>
-         {props.users.map(u => <div key={u.id}>
+      <div className={styles.users}>
+
+         <button onClick={getUsers}>Get Users</button>
+
+         {props.users.map(u => <div className={styles.user} key={u.id}>
             <span>
                <div>
                   <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
