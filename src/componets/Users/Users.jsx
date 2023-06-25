45f1a67b581a44f7383aa6c79fdfc7053a6738a1
snitovlad <snitovov@gmail.com>
2023-06-26 00:09:02 +0300

    Lesson 62. follow-unfollow API

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -1,6 +1,7 @@
 import styles from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 
 let Users = (props) => {
@@ -22,23 +23,48 @@ let Users = (props) => {
          <div>
             {slicedCurPage.map(p => {
                return <span className={props.currentPage === p ? styles.selectedPage : styles.noSelectedPage}
-                  onClick={(event) => { props.onPageChanged(p) } } key={p}> {p} </span> 
+                  onClick={(event) => { props.onPageChanged(p) }} key={p}> {p} </span>
             })
             }
-            <span>    всего страниц  { pagesCount }</span>
+            <span>    всего страниц  {pagesCount}</span>
          </div>
 
          {props.users.map(u => <div className={styles.user} key={u.id}>
             <span>
                <div>
                   <NavLink to={'/profile/' + u.id}>
-                  <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+                     <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                   </NavLink>
                </div>
                <div>
                   {u.followed ?
-                     <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button> :
-                     <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                     <button onClick={() => {
+                        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                           withCredentials: true,
+                           headers: {
+                              "API-KEY": "5208d8eb-4483-4f95-a912-098579a41e05"
+                           }
+                        }) //т.к. кроссдоменный запрос delete - то не нужен дополнительный параметр {}
+                           .then(response => {
+                              if (response.data.resultCode === 0) {
+                                 props.unfollow(u.id)
+                              }
+                           })
+                     }}>Unfollow</button> :
+
+                     <button onClick={() => {
+                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                           withCredentials: true,
+                           headers: {
+                              "API-KEY": "5208d8eb-4483-4f95-a912-098579a41e05"
+                           }
+                        }) //т.к. кроссдоменный запрос post - то нужен дополнительный параметр {}
+                           .then(response => {
+                              if (response.data.resultCode === 0) {
+                                 props.follow(u.id)
+                              }
+                           })
+                     }}>Follow</button>}
                </div>
             </span>
             <span>
@@ -49,7 +75,7 @@ let Users = (props) => {
                <div>{'u.location.country'}</div>
                <div>{'u.location.city'}</div>
             </span>
-         </div> )}
+         </div>)}
 
       </div>
    )
