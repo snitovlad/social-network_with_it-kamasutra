8544d7a69a0977e3ed77c85d4c054df3c2b6f6da
snitovlad <snitovov@gmail.com>
2023-09-10 15:56:14 +0300

    Lesson 85. useEffect

--- src/componets/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/componets/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const ProfileStatusWithHooks = (props) => {
 
    let [editMode, setEditMode] = useState(false);
+   let [status, setStatus] = useState(props.status);
+
+   useEffect( () => {
+      setStatus(props.status)
+   }, [props.status]);  //зависимость вызова useEffect
+
+
    const activateEditMode = () => {
       setEditMode(true);
    }
+
    const deactivateEditMode = () => {
       setEditMode(false);
       props.updateStatus(status); //отправили статус на сервер
    }
 
-   let [status, setStatus] = useState(props.status);
    const onStatusChange = (event) => {
       setStatus (event.currentTarget.value);
    }
